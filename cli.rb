c1d756e630c7d70c8bcb672598efaacae2a798ba
Leonardo Prado <leonardoprado@gmail.com>
2022-09-27 16:03:12 -0300

    Isola comportamento do CLI numa classe específica
    Signed-off-by: Leonardo Prado <leonardoprado@gmail.com>

--- cli.rb
+++ cli.rb
@@ -1,45 +1,38 @@
 module Cli
-  def self.included(base)
-    base.class_eval do
-      include TTY::Option
+  include TTY::Option
 
-      usage do
-        no_command
+  usage do
+    no_command
 
-        desc "Valida o tempo de preparo do miojo usando duas ampulhetas"
+    desc "Valida o tempo de preparo do miojo usando duas ampulhetas"
 
-        example "miojo 3 5 7"
-      end
+    example "miojo 3 5 7"
+  end
 
-      argument :miojo do
-        required
-        convert :int
-        validate { _1 > 0 }
-        desc "O tempo de cozimento do miojo."
-      end
+  argument :miojo do
+    required
+    convert :int
+    validate { _1 > 0 }
+    desc "O tempo de cozimento do miojo."
+  end
 
-      argument :ampulheta1 do
-        required
-        convert :int
-        validate { _1 > 0 && _1 < params[:miojo] }
-        desc "Tempo da primeira ampulheta."
-      end
+  argument :ampulheta1 do
+    required
+    convert :int
+    validate { _1 > 0 && _1 < params[:miojo] }
+    desc "Tempo da primeira ampulheta."
+  end
 
-      argument :ampulheta2 do
-        required
-        convert :int
-        validate { _1 > 0 && _1 < params[:miojo] }
-        desc "Tempo da segunda ampulheta."
-      end
+  argument :ampulheta2 do
+    required
+    convert :int
+    validate { _1 > 0 && _1 < params[:miojo] }
+    desc "Tempo da segunda ampulheta."
+  end
 
-      flag :help do
-        short "-h"
-        long "--help"
-        desc "Exibe o texto de ajuda"
-      end
-    end
+  flag :help do
+    short "-h"
+    long "--help"
+    desc "Exibe o texto de ajuda"
   end
 end
-
-
-
